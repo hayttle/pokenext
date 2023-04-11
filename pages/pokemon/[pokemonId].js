19b22d372f4990a19776bb9c41f11830da724252
hayttle <hayttle@gmail.com>
2023-04-11 15:05:37 -0300

    falback true

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import styles from "@/styles/Pokemon.module.css"
+import {useRouter} from "next/router"
 
 export const getStaticPaths = async () => {
   const maxPokemons = 251
@@ -15,7 +16,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -30,6 +31,12 @@ export const getStaticProps = async (context) => {
 }
 
 export default function Pokemon({pokemon}) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <dir>Carregando...</dir>
+  }
+
   return (
     <div className={styles.pokemon_container}>
       <h1 className={styles.title}>{pokemon.name}</h1>
